1787978eea8867f70ecf678c1f6ff2a845ca3dcf
stepga <stepga@nirgendwo.eu>
2025-02-16 00:49:24 +0100

    nixos: keymap us altgr-intl

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -49,8 +49,9 @@
   };
 
   # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+  services.xserver.xkb.layout = "us";
+  services.xserver.xkb.variant = "altgr-intl";
+  services.xserver.xkb.options = "eurosign:e,caps:escape";
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
