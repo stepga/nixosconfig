aed29332c37d3379f2af6bb099a56d3269e7cec5
stepga <stepga@nirgendwo.eu>
2025-03-01 23:43:16 +0100

    directory re-structuring

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
 
-            home-manager.users."${variables.username}" = import ./home.nix;
+            home-manager.users."${variables.username}" = import ./home/home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
           }
