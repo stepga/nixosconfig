bf67f34acf64c6dffcf7f00e97a34c3f6fbe1a78
Stephan Gabert <stepga@nirgendwo.eu>
2026-05-04 17:11:00 +0200

    home.nix: firefox: allow extensions in private browsing mode

--- home/home.nix
+++ home/home.nix
@@ -199,6 +199,7 @@
           value = {
             install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
             installation_mode = "normal_installed";
+            private_browsing = true;
           };
         };
         # find addon short ID example:
